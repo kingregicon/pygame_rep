449fe3c82611f8a8e6ceeabc49ccd3d17a121ec8
kingregicon <44660803+kingregicon@users.noreply.github.com>
2019-03-19 16:15:55 -0700

    First Push

---
+++ dvd_bounce.py
@@ -0,0 +1,24 @@
+import pygame, time
+
+pygame.init()
+width, height = 800,  600
+dvd_logo_speed = [1, 1]
+backgroundColor = 0,  0,  0
+
+# dvd_logo_path = "C:\Users\Sam\Google Drive\My Projects\PythonStuff\pygame_tests\dvd_bounce"
+
+
+screen = pygame.display.set_mode((width, height))
+
+# Asset - the DVD Logo
+
+dvd_logo = pygame.image.load("dvd_logo.png")
+
+dvd_logo_r = dvd_logo.get_rect()
+
+while True:
+    screen.fill(backgroundColor)
+
+    screen.blit(dvd_logo, dvd_logo_r)
+    pygame.display.flip()
+    time.sleep(10 / 1000)
