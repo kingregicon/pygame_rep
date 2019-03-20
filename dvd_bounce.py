d8d1f737cfda89fc976d163c1823fb5efe4b60ad
Ben Wurth <ben.wurth@gmail.com>
2019-03-19 21:40:46 -0700

    Adds an event loop

--- dvd_bounce.py
+++ dvd_bounce.py
@@ -1,4 +1,5 @@
-import pygame, time
+import pygame
+import time
 
 pygame.init()
 width, height = 800,  600
@@ -16,9 +17,13 @@ dvd_logo = pygame.image.load("dvd_logo.png")
 
 dvd_logo_r = dvd_logo.get_rect()
 
-while True:
-    screen.fill(backgroundColor)
+running = True
 
+while running:
+    screen.fill(backgroundColor)
     screen.blit(dvd_logo, dvd_logo_r)
     pygame.display.flip()
     time.sleep(10 / 1000)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
